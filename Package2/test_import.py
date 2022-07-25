84c1311faabc958e97db8e31e2e3b3c247972d10
TranTuanBinh <tuanbinh.work@gmail.com>
2022-07-25 12:46:42 +0700

    abda

--- Package2/test_import.py
+++ Package2/test_import.py
@@ -1,5 +1,10 @@
-#absolute path
-#from Package2.function_practice import func_6
+#1 write a python program to read file sample.json. display all distance information and name of location 
+import json
 
-#relative path
 
+def op_1():
+    with open('sample.json', 'r', encoding='utf-8') as a:
+        obj = json.load(a)
+        print(obj)
+
+op_1()
