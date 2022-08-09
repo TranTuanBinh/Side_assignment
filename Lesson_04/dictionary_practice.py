ffd7e6e41fe87be297c0d4e8ccc9eb2f0be54da3
TranTuanBinh <tuanbinh.work@gmail.com>
2022-08-09 16:23:29 +0700

    lesson 4

--- Lesson_04/dictionary_practice.py
+++ Lesson_04/dictionary_practice.py
@@ -6,12 +6,14 @@ a_dict = dict(zip(key, values))
 
 print('Dictionary after convert:', a_dict)
 print('------------------')
+
 #2: Frome two dictionaries, merge into one
 dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
 dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
 
 dict3 = dict1.copy ()
 dict3.update(dict2)
+
 print(dict3)
 print('-----------------')
 #3:Print the value of key 'physics' from the below dict
@@ -26,13 +28,16 @@ sampleDict = {
         }
     }
 }
+
 print('the value of key physics is', sampleDict['class']['student']['marks']['physics'])
 print('-----------------')
+
 #4:Initialize dictionary with default values
 employees = ['Kelly', 'Emma']
 defaults = {"designation": 'Developer', "salary": 8000}
 
 ex4_dict = dict.fromkeys(employees, defaults)
+
 print(ex4_dict)
 print('---------------')
 
@@ -44,20 +49,16 @@ sample_dict1 = {
     "city": "New york"}
 
 keys_extract = ["name", "salary"]
+
 ex5_dict ={}
 for i in keys_extract:
     ex5_dict[i]=sample_dict1[i]
 
 
 print('new dict after extracting: ',ex5_dict)
-
 print('-----------------')
 
-
-
-
 #6:Delete a list of keys from a dictionary
-
 sample_dict2 = {
     "name": "Kelly",
     "age": 25,
@@ -79,24 +80,22 @@ for x in sample_dict3.values():
     if x == 200: 
         print('True')
     
-
-
 print('-----------------')
 
 #8:Rename a key city to a location in the following dictionary
-
 sample_dict4 = {
   "name": "Kelly",
   "age":25,
   "salary": 8000,
   "city": "New york"
 }
+
 sample_dict4['location'] = sample_dict4.pop('city')
+
 print(sample_dict4.items())
-
 print('-------------------------')
-#9:Get the key of a minimum value from the following dictionary
 
+#9:Get the key of a minimum value from the following dictionary
 sample_dict5 = {
   'Physics': 10,
   'Math': 65,
@@ -109,13 +108,14 @@ for x in sample_dict5.values():
 
 print(min(min_in_dict))
 print('-------------------------')
-#10:Change Brad’s salary to 8500 in the following dictionary.
 
+#10:Change Brad’s salary to 8500 in the following dictionary.
 sample_dict6 = {
     'emp1': {'name': 'Jhon', 'salary': 7500},
     'emp2': {'name': 'Emma', 'salary': 8000},
     'emp3': {'name': 'Brad', 'salary': 500}
 }
+
 for k1,v1 in sample_dict6.items():
      for k2,v2 in v1.items():
         if v2 == 'Brad': 
